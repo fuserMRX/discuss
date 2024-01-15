b860a40b66b77fab5fe3e28d00e254e40f10b570
ast_vnovodid <v.novodid@astoundcommerce.com>
2024-01-15 12:30:49 +0200

    feat: add server and client authorization

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,5 @@
 import NextAuth from 'next-auth';
-import GitHub from '@auth/core/providers/github';
+import Github from 'next-auth/providers/github';
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import { db } from '@/db';
 
@@ -7,26 +7,30 @@ const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
 const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 
 if (!GITHUB_CLIENT_ID || !GITHUB_CLIENT_SECRET) {
-    throw new Error('Missing github oauth credentials');
+  throw new Error('Missing github oauth credentials');
 }
 
-export const { handlers: { GET, POST }, auth, signOut, signIn } = NextAuth({
-    adapter: PrismaAdapter(db),
-    providers: [
-        GitHub({
-            clientId: GITHUB_CLIENT_ID,
-            clientSecret: GITHUB_CLIENT_SECRET
-        })
-    ],
-    callbacks: {
-        // who are user is in our application
-        // usually not needed - bug
-        async session({ session, user }: any) {
-            if (session && user) {
-                session.user.id = user.id;
-            }
+export const {
+  handlers: { GET, POST },
+  auth,
+  signOut,
+  signIn,
+} = NextAuth({
+  adapter: PrismaAdapter(db),
+  providers: [
+    Github({
+      clientId: GITHUB_CLIENT_ID,
+      clientSecret: GITHUB_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    // Usually not needed, here we are fixing a bug in nextauth
+    async session({ session, user }: any) {
+      if (session && user) {
+        session.user.id = user.id;
+      }
 
-            return session;
-        }
-    }
-})
+      return session;
+    },
+  },
+});
