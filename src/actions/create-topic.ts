73a7e2227efbda595ea8db2e1ff2a8c65002565a
ast_vnovodid <v.novodid@astoundcommerce.com>
2024-01-16 14:29:57 +0200

    feat: handling DB erros in forms

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,7 +1,12 @@
 'use server';
 
+import type { Topic } from '@prisma/client';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 import { z } from 'zod';
 import { auth } from '@/auth';
+import { db } from '@/db';
+import paths from '@/paths';
 
 const createTopicSchema = z.object({
     name: z.string().min(3).regex(/^[a-z-]+$/, { message: 'Must be letters or dashes without spaces' }),
@@ -37,7 +42,30 @@ export async function createTopic(formState: CreateTopicFormState, formData: For
         }
     }
 
-    return {
-        errors: {},
+    let topic: Topic;
+    try {
+        topic = await db.topic.create({
+            data: {
+                slug: result.data.name,
+                description: result.data.description
+            }
+        })
+    } catch (err: unknown) {
+        if (err instanceof Error) {
+            return {
+                errors: {
+                    _form: [err.message]
+                }
+            }
+        } else {
+            return {
+                errors: {
+                    _form: ['Something went wrong']
+                }
+            }
+        }
     }
+
+    revalidatePath('/');
+    redirect(paths.topicShow(topic.slug));
 }
