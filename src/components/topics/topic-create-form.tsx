1e1d8023daa4d372c3accddeaf507cbd2b4416c9
ast_vnovodid <v.novodid@astoundcommerce.com>
2024-01-16 12:18:28 +0200

    feat: receiving form data

--- src/components/topics/topic-create-form.tsx
+++ src/components/topics/topic-create-form.tsx
@@ -15,8 +15,8 @@ export default function TopicCreateForm() {
                 <form action={actions.createTopic}>
                     <div className='flex flex-col gap-4 p-4 w-80'>
                         <h3 className='text-lg'>Create a Topic</h3>
-                        <Input label='Name' labelPlacement='outside' placeholder='Name' />
-                        <Textarea label='Description' labelPlacement='outside' placeholder='Describe your topic' />
+                        <Input name='name' label='Name' labelPlacement='outside' placeholder='Name' />
+                        <Textarea name='description' label='Description' labelPlacement='outside' placeholder='Describe your topic' />
                     </div>
                     <Button type='submit'>Submit</Button>
                 </form>
