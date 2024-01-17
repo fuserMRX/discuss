37c2ab957bf45f540edb7a5df5624fb58c73cdb8
ast_vnovodid <v.novodid@astoundcommerce.com>
2024-01-17 11:26:57 +0200

    feat: passing additional args to a server aciton

--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -15,7 +15,7 @@ export default function TopicShowPage({ params }: TopicShowPageProps) {
             </h1>
         </div>
         <div>
-            <PostCreateForm />
+            <PostCreateForm slug={slug} />
         </div>
     </div>
 } 
